7387beda024026d39594561506f41958435a56f8
Jae Won Choi <45551860+cjw94103@users.noreply.github.com>
2024-11-25 08:15:08 +0900

    Update evaluation.py

--- FasterRCNN/evaluation.py
+++ FasterRCNN/evaluation.py
@@ -2,6 +2,7 @@ import torch
 import numpy as np
 import cv2
 import argparse
+import pandas as pd
 
 from torchvision import models
 from torchvision import ops
@@ -10,7 +11,7 @@ from torchvision.models.detection import FasterRCNN
 from torchvision.models.detection.backbone_utils import resnet_fpn_backbone
 
 from coco_dataset import COCODataset
-from transform_util import PILToTensor, ToDtype
+from transform_utils import PILToTensor, ToDtype, Compose
 from torch.utils.data import DataLoader
 
 from utils import *
@@ -20,54 +21,51 @@ from tqdm import tqdm
 from eval_utils.metric import get_inference_metrics_from_df, summarise_inference_metrics
 from eval_utils.coco_metric import get_coco_from_dfs
 
-import pandas as pd
-
-# Define a custom argument type for a list of integers
+## argparse
 def list_of_ints(arg):
     return list(map(int, arg.split(',')))
 
 def list_of_floats(arg):
     return list(map(float, arg.split(',')))
 
-# argparse
 parser = argparse.ArgumentParser()
 
 ## prepare dataset
-parser.add_argument("--data_path", type=str, help="your custom dataset path", default="./data/coco2017/")
+parser.add_argument("--data_path", type=str, help="your custom dataset path", default="/data/02_COCOData/")
 
 ## data generator
 parser.add_argument("--num_workers", type=int, help="num workers of generator", default=0)
-parser.add_argument("--batch_size", type=int, help="num of batch size", default=4)
+parser.add_argument("--batch_size", type=int, help="num of batch size", default=8)
 
 ## model architecture
 parser.add_argument("--backbone", type=str, help="backbone of faster rcnn", default='vgg16')
-parser.add_argument('--anchor_sizes', type=list_of_ints, help="anchor size of faster rcnn", default="1, 2, 3, 4, 5, 6")
-parser.add_argument('--anchor_ratio', type=list_of_floats, help="anchor aspect ratio of faster rcnn", default="0.5, 1, 2")
+parser.add_argument('--anchor_sizes', type=list_of_ints, help="anchor size of faster rcnn", default="32, 64, 128, 256, 512")
+parser.add_argument('--anchor_ratio', type=list_of_floats, help="anchor aspect ratio of faster rcnn", default="0.5, 1.0, 2.0")
 parser.add_argument('--pooler_output_size', type=list_of_ints, help="pooler output size of faster rcnn", default="7, 7")
 parser.add_argument("--pooler_sampling_ratio", type=int, help="pooler sampling ratio", default=2)
 
 ## Model save
-parser.add_argument("--model_save_path", type=str, help="your model save path", default="./model_result/02_Aug_VGG_Backbone/Augment_VGG_model.pth")
+parser.add_argument("--model_save_path", type=str, help="your model save path", default="./model_result/VGG16_backbone/FasterRCNN_VGG_model.pth")
+
+args = parser.parse_args()
 
 ## make dataloader
 def collator(batch):
     return tuple(zip(*batch))
 
 val_transform = Compose(
-    [
-        PILToTensor(),
-        ToDtype(scale=True, dtype=torch.float)
-    ]
-)
-val_dataset = COCODataset_Aug(args.data_path, train=False, transform=val_transform)
+        [
+            PILToTensor(),
+            ToDtype(scale=True, dtype=torch.float)
+        ]
+    )
+val_dataset = COCODataset(args.data_path, train=False, transform=val_transform)
 val_dataloader = DataLoader(
     val_dataset, batch_size=args.batch_size, shuffle=True, drop_last=True, collate_fn=collator, num_workers=args.num_workers
 )
 
 ## load trained model
-weights_path = args.model_save_path
-weights = torch.load(weights_path)
-device = "cuda" if torch.cuda.is_available() else "cpu"
+weigths = torch.load(args.model_save_path)
 
 if args.backbone == 'vgg16':
     backbone = models.vgg16(weights="VGG16_Weights.IMAGENET1K_V1").features
@@ -79,27 +77,32 @@ if args.backbone == 'vgg16':
     model = FasterRCNN(backbone=backbone, 
                        num_classes=len(val_dataset._get_categories()), 
                        rpn_anchor_generator=anchor_generator, 
-                       box_roi_pool=roi_pooler ).to(device)
+                       box_roi_pool=roi_pooler ).to("cuda")
+    
+    model.load_state_dict(weigths)
+    model.eval()
     
 elif args.backbone == 'resnet50fpn':
     backbone = resnet_fpn_backbone('resnet50', pretrained=True)
-    model = FasterRCNN(backbone, num_classes=len(val_dataset._get_categories())).to('cuda')
+    model = FasterRCNN(backbone, num_classes=len(val_dataset._get_categories())).to("cuda")
+    
+    model.load_state_dict(weigths)
+    model.eval()
 
-model = model.load_state_dict(weights)
-model = model.to(device)
-model.eval()
+## inference
+_cate_dict = val_dataset.new_categories
+cate_dict = {}
+for key, value in _cate_dict.items():
+    cate_dict[value] = key
 
-## Evaluate
 gt_lists = []
 pred_lists = []
-stop_flag = 0
 
 with torch.no_grad():
     model.eval()
     fileidx = 1
-    # stop_flag += 1
-    for images, targets in tqdm(val_dataloader, total=len(val_dataloader)):
-        images = [img.to(device) for img in images]
+    for images, targets, _ in tqdm(val_dataloader, total=len(val_dataloader)):
+        images = [img.to("cuda") for img in images]
         outputs = model(images)
         
         # filename (arbitraily)
@@ -127,7 +130,7 @@ with torch.no_grad():
             pred_list = [x_min, y_min, x_max, y_max, cate_dict[pred_label], pred_score, filename]
             pred_lists.append(pred_list)
 
-# make dataframe
+## result score
 gt_lists = np.array(gt_lists)
 pred_lists = np.array(pred_lists)
 
@@ -148,5 +151,6 @@ labels_df['ymax'] = np.array(gt_lists[:,3], dtype=np.float32)
 labels_df['label'] = gt_lists[:,4]
 labels_df['image_name'] = gt_lists[:,5]
 
-# extract evaluation score
 res = get_coco_from_dfs(preds_df, labels_df, False)
+
+print(res)
